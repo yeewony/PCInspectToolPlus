41717a13732428742047c2d23e09f8a48f469c19
yeewony <pandorasb0x@naver.com>
2021-09-29 21:17:19 +0900

    add 1

---
+++ SecuCheck7/P03_AdministrativeShare.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SecuCheck7
+{
+    public class P03_AdministrativeShare : Auto
+    {
+       
+    }
+}
